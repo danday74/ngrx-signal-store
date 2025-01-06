df154fdeb9ba97d07bc1468ed95ce1889e3cdf31
danday74 <daniellewis777@gmail.com>
2025-01-06 22:11:23 +0000

    services

---
+++ src/app/services/users/users.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { IUser } from '@interfaces/i-user'
+import { HttpClient } from '@angular/common/http'
+import { myAppConfig } from '../../my-app-config'
+
+@Injectable({ providedIn: 'root' })
+export class UsersService {
+  private readonly http: HttpClient = inject(HttpClient)
+
+  loadUser(uid: number): Observable<IUser> {
+    return this.http.get<IUser>(`https://dummyjson.com/users/${uid}?delay=${myAppConfig.delay}`)
+  }
+}
