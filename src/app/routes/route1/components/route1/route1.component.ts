ba1f22ba4a3b6d4b3945930de01ba32bab565136
danday74 <daniellewis777@gmail.com>
2025-01-06 23:48:27 +0000

    route1

--- src/app/routes/route1/components/route1/route1.component.ts
+++ src/app/routes/route1/components/route1/route1.component.ts
@@ -1,13 +1,23 @@
 import { Component, inject } from '@angular/core'
 import { UsersStore } from '@stores/users.store'
 import { CommonModule } from '@angular/common'
+import { TSortProp } from '@ttypes/t-sort-prop'
+import { CasePipe } from '@pipes/case/case.pipe'
 
 @Component({
   selector: 'app-route1',
-  imports: [CommonModule],
+  imports: [CommonModule, CasePipe],
   templateUrl: './route1.component.html',
   styleUrl: './route1.component.scss',
 })
 export class Route1Component {
   readonly usersStore = inject(UsersStore)
+
+  orderBy(prop: string) {
+    this.usersStore.orderBy(prop as TSortProp)
+  }
+
+  activate(id: number) {
+    this.usersStore.setActiveUser(id)
+  }
 }
