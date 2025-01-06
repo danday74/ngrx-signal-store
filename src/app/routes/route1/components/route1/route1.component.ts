775132547ea7a7722e75264b6914ff67039a0404
danday74 <daniellewis777@gmail.com>
2025-01-06 15:36:37 +0000

    users

--- src/app/routes/route1/components/route1/route1.component.ts
+++ src/app/routes/route1/components/route1/route1.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core'
 import { UsersStore } from '@stores/users.store'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-route1',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './route1.component.html',
   styleUrl: './route1.component.scss',
 })
--- src/app/stores/users.store.ts
+++ src/app/stores/users.store.ts
@@ -1,12 +1,13 @@
 import { signalStore, withState } from '@ngrx/signals'
 import { IUser } from '@interfaces/i-user'
+import { users } from '@data/users'
 
 interface UsersState {
   users: IUser[]
 }
 
 const initialState: UsersState = {
-  users: [],
+  users,
 }
 
 export const UsersStore = signalStore(
