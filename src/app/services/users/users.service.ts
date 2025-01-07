8938d3de1ad4f860db63542423214b03908e19c5
danday74 <daniellewis777@gmail.com>
2025-01-07 00:40:48 +0000

    users service

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,12 +1,21 @@
 import { inject, Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
+import { map, Observable } from 'rxjs'
 import { IUser } from '@interfaces/i-user'
 import { HttpClient } from '@angular/common/http'
 import { myAppConfig } from '../../my-app-config'
+import { IUsersResponse } from '@interfaces/i-users-response'
 
 @Injectable({ providedIn: 'root' })
 export class UsersService {
   private readonly http: HttpClient = inject(HttpClient)
+
+  loadUsers(): Observable<IUser[]> {
+    return this.http.get<IUsersResponse>(`https://dummyjson.com/users?limit=6&delay=${myAppConfig.delay}`).pipe(
+      map((res: IUsersResponse) => {
+        return res.users
+      }),
+    )
+  }
 
   loadUser(id: number): Observable<IUser> {
     return this.http.get<IUser>(`https://dummyjson.com/users/${id}?delay=${myAppConfig.delay}`)
