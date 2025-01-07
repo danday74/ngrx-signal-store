4208bbbdc8ee6e00512866bda7ba1d609088b125
danday74 <daniellewis777@gmail.com>
2025-01-07 01:35:22 +0000

    remember user

--- src/app/routes/route1/components/route1/route1.component.ts
+++ src/app/routes/route1/components/route1/route1.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core'
+import { Component, computed, effect, inject, input, InputSignal, Signal } from '@angular/core'
 import { UsersStore } from '@stores/users.store'
 import { CommonModule } from '@angular/common'
 import { TSortProp } from '@ttypes/t-sort-prop'
 import { CasePipe } from '@pipes/case/case.pipe'
 import { UserComponent } from '@components/user/user.component'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-route1',
@@ -12,13 +13,25 @@ import { UserComponent } from '@components/user/user.component'
   styleUrl: './route1.component.scss',
 })
 export class Route1Component {
+  paramId: InputSignal<string> = input<string>(null) // route param
+
   readonly usersStore = inject(UsersStore)
+
+  private id: Signal<number> = computed<number>((): number => parseInt(this.paramId(), 10))
+
+  private router: Router = inject(Router)
+
+  constructor() {
+    effect(() => {
+      this.usersStore.setActiveUser(this.id())
+    })
+  }
 
   orderBy(prop: string) {
     this.usersStore.orderBy(prop as TSortProp)
   }
 
   setActiveUser(id: number) {
-    this.usersStore.setActiveUser(id)
+    this.router.navigateByUrl(`/route1/${id}`).then()
   }
 }
--- src/app/routes/route1/routes.ts
+++ src/app/routes/route1/routes.ts
@@ -3,6 +3,10 @@ import { Route1Component } from './components/route1/route1.component'
 
 export const route1Routes: Route[] = [
   {
+    path: ':paramId',
+    component: Route1Component,
+  },
+  {
     path: '',
     component: Route1Component,
   },
